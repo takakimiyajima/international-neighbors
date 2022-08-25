89f79dc2db227e6aebff75189cb6a249b44a32b3
Takaki Miyajima <takaki.mz@gmail.com>
2022-08-25 15:33:19 -0700

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'vue/script-setup-no-uses-vars': 'off'
+  },
+}
