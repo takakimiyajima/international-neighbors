ee268bfe1d47f38511fb1e3b7fe568b783ed6894
Takaki Miyajima <takaki.mz@gmail.com>
2022-09-02 22:03:27 -0700

    removed unnecessary codes

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,10 @@
 <template>
   <div>
     <NuxtLayout />
-    <Suspense>
-      <template #default>
-        <div class="container" style="padding: 50px 0 100px 0">
-          <Account v-if="currentUser" />
-          <Auth v-else />
-        </div>
-      </template>
-      <template #fallback>
-        <span>読み込み中…</span>
-      </template>
-    </Suspense>
+    <div class="container" style="padding: 50px 0 100px 0">
+      <Account v-if="currentUser" />
+      <Auth v-else />
+    </div>
   </div>
 </template>
 
@@ -19,11 +12,4 @@
 import { useAuth } from '@/composables/useAuth'
 
 const { currentUser } = useAuth()
-const router = useRouter()
-
-onMounted(() => {
-  if (!currentUser.value) {
-    router.replace('/signin')
-  }
-})
 </script>
