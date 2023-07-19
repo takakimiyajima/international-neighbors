bff238c842389825a9d898a5315fc996a06b1945
Takaki Miyajima <takaki.mz@gmail.com>
2023-07-20 00:22:46 +0900

    style: theme of vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,11 +2,30 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    primary: '#1976D2',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+};
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     createVuetify({
       components,
       directives,
+      theme: {
+        defaultTheme: 'myCustomLightTheme',
+        themes: {
+          myCustomLightTheme,
+        },
+      },
     })
   )
 })
